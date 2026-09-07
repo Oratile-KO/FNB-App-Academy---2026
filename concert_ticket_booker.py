346ddfd6351360ab0591f6c43f2acc9144c15f17
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-07 19:55:50 +0200

    Create concert_ticket_booker.py
    The Challenge: “The Concert Ticket Booker”

---
+++ concert_ticket_booker.py
@@ -0,0 +1,5 @@
+#Prompt user for input
+name = input("Enter your name: ")
+artist  = input("Which artist or band do you want to see: ")
+
+print(f"Hi, {name}! Your ticket(s) to see {artist} are booked successfully!")
