088395a2932c5f26496033bdf29af0b41e68557b
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-10 01:26:11 +0200

    Update contact_book.py
    Add, view and menu complete

--- contact_book.py
+++ contact_book.py
@@ -1,44 +1,40 @@
-#Prompt user to make a selection from the menu options
-action = int(input("Please select an Option: \n1. Add \n2. Search \n3. Delete \n4. View All \n5. Exit \n").strip())
+#Declare and inittialize the contact book dictionary
+contacts = {"name" : [], "phone" : [], "email" : []}
 
-#Declare the dictionary
-contacts = {'name': '', 'phone' : '', 'email': '' }
-name = ''
-
-#Handle user selection input using functions
+#Implement an add_contact() function that appends a new dictionary to the list
 def add_contact():
-    name = input("Enter the contact's name: ").strip()
-    contacts['name'] = name
-    phone_number = int(input("Enter the contact's phone number: ").strip())
-    contacts['phone'] = phone_number
-    email = input("Enter the contact's email address: ").strip()
-    contacts['email'] = email
+    name = input("Add the name of the contact: ").strip().lower()
+    contacts["name"].append(name)
+    phone = int(input("Add the contact's phone number: ").strip())
+    contacts["phone"].append(phone)
+    email = input("Add the contact's email address: ").strip().lower()
+    contacts["email"].append(email)
 
-def contact_name(name):
-    search_name = input("Enter the name you would like to search: ").strip().lower()
-    print(f"{contacts.get(search_name, 'Name not found!')}")
+# Implement a search_contact(name) function that searches by name and returns the matching dictionary (or None if not found)
+def search_contact(name):
+    search_name = input("Enter the name of the contacts you wouldlike to search: ").strip().lower()
 
+#Implement a delete_contact(name) function that removes a contact by name
+def delete_contact(name):
+    delete_name = input("Enter the name of the contact you would like to delete: ").strip().lower()
+
+#Implement a view_all() function that displays all contacts in a formatted layout
 def view_all():
     for key, value in contacts.items():
         print(key, value)
 
-def delete_contact(name):
-    delete_name = input("Enter the name of the contact you would like to delete: ").strip().lower()
-    del contacts[delete_contact]
-
+#Use a while loop menu to let the user choose an action (1=Add, 2=Search, 3=Delete, 4=View All, 5=Exit)
 while True:
+    action = int(input("Please select an option below: \n1. Add \n2. Search \n3. Delete \n4. View All \n5. Exit \n").strip())
     if action == 1:
         add_contact()
-
     elif action == 2:
-        contact_name(name)
-
+        search_contact(name)
     elif action == 3:
         delete_contact(name)
-
     elif action == 4:
         view_all()
-        break
-
     elif action == 5:
         break
+    else:
+        print("Invalid option selected, please try again!")
