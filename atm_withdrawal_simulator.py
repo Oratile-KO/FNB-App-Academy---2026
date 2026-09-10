e9e79fd34f0c55f13df79aa39341d3127686b9b7
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-10 19:08:13 +0200

    Create atm_withdrawal_simulator.py
    The Challenge: “The Smart ATM Withdrawal Simulator”
    Simulate a bank transaction checking if a user has enough money.
    
    1. Set a fixed variable representing a bank balance, for example: balance = 500.
    
    2. Ask the user how much money they want to withdraw. (Remember to cast it to an integer or float!).
    
    3. If the request is less than or equal to the balance, deduct the amount and print:
    “Withdrawal successful! Remaining balance: RX”.
    
    4. But what if they try to withdraw a negative amount or zero? Add an elif statement checking if the request is less than or equal to 0. If so, print: “Invalid amount”. You must withdraw more than “R0”.
    
    5. Otherwise (else), print: “Declined. Insufficient funds”

---
+++ atm_withdrawal_simulator.py
@@ -0,0 +1,16 @@
+#Set a fixed variable representing a bank balance
+balance = 500
+
+#Prompt user to withdraw
+withdrawal = int(input("How much would you like to withdraw? "))
+
+#Handle negative or zero input
+if withdrawal <= 0:
+    print("Invalid amount. You must withdraw more than R0")
+
+elif withdrawal <= balance:
+    balance -= withdrawal
+    print(F"Withdrawal successful! Remaining balance: R{balance}")
+
+else:
+    print("Declined. Insufficient funds")
