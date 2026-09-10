bac5e145081ea34dbff4df9a9500d659c56b3d63
Oratile-KO <162906040+Oratile-KO@users.noreply.github.com>
2026-09-10 15:29:57 +0200

    Update contact_book.py
    Contacts fixed as list of dictionaries
    add contact function adjusted

--- contact_book.py
+++ contact_book.py
@@ -1,29 +1,36 @@
 #Declare and inittialize the contact book dictionary
-contacts = {"name" : [], "phone" : [], "email" : []}
+contacts = []
 
-#Implement an add_contact() function that appends a new dictionary to the list
+# name = ''
+
+# #Implement an add_contact() function that appends a new dictionary to the list
 def add_contact():
     name = input("Add the name of the contact: ").strip().lower()
-    contacts["name"].append(name)
     phone = int(input("Add the contact's phone number: ").strip())
-    contacts["phone"].append(phone)
     email = input("Add the contact's email address: ").strip().lower()
-    contacts["email"].append(email)
+    new_contact = { 'name': name, 'phone': phone, 'email': email}
 
-# Implement a search_contact(name) function that searches by name and returns the matching dictionary (or None if not found)
-def search_contact(name):
-    search_name = input("Enter the name of the contacts you wouldlike to search: ").strip().lower()
+    contacts.append(new_contact)
 
-#Implement a delete_contact(name) function that removes a contact by name
-def delete_contact(name):
-    delete_name = input("Enter the name of the contact you would like to delete: ").strip().lower()
+# #Implement a search_contact(name) function that searches by name and returns the matching dictionary (or None if not found)
+# def search_contact(name):
+#     search_name = input("Enter the name of the contacts you wouldlike to search: ").strip().lower()
+#     for searched_contact in contacts:
+#         if contacts["name"] == search_name:
+#             return searched_contact
+#         print(search_contact(search_name))
+
+# #Implement a delete_contact(name) function that removes a contact by name
+# def delete_contact(name):
+#     delete_name = input("Enter the name of the contact you would like to delete: ").strip().lower()
 
 #Implement a view_all() function that displays all contacts in a formatted layout
 def view_all():
-    for key, value in contacts.items():
-        print(key, value)
+    for contact_list in contacts:
+        for key, value in contact_list.items():
+            print(key, value, "")
 
-#Use a while loop menu to let the user choose an action (1=Add, 2=Search, 3=Delete, 4=View All, 5=Exit)
+# # #Use a while loop menu to let the user choose an action (1=Add, 2=Search, 3=Delete, 4=View All, 5=Exit)
 while True:
     action = int(input("Please select an option below: \n1. Add \n2. Search \n3. Delete \n4. View All \n5. Exit \n").strip())
     if action == 1:
